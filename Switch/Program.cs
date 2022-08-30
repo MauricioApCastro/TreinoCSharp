bed3c725a71917b378f4a53b4e4ff58c40780353
MauricioApCastro <castromac6367@gmail.com>
2022-08-29 23:22:22 -0300

    exemplo switch

---
+++ Switch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       Escolha um opção      ");
+            Console.WriteLine("       1- entrar             ");
+            Console.WriteLine("       2- próxima tela       ");
+            Console.WriteLine("       3- tela anterior      ");
+            Console.WriteLine("       4- sair               ");
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("Você escolheu opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu opção 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Você escolheu opção 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Você escolheu opção 4");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+    }
+}
