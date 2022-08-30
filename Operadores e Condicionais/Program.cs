989bdbe6acf927969504c936b7bd5427cd22a1e2
MauricioApCastro <castromac6367@gmail.com>
2022-08-29 23:07:59 -0300

    exemplo de IF

---
+++ Operadores e Condicionais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Operadores_e_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha um número");
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("O número é par");
+
+            }
+            else
+            {
+                Console.WriteLine("O número é impar");
+            }
+        }
+    }
+}
