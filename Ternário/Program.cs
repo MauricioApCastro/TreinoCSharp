4eb6242789c971e7ceabd343300adb9b61ef9a0c
MauricioApCastro <castromac6367@gmail.com>
2022-08-29 23:29:31 -0300

    Exemplo de ternário

---
+++ Ternário/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ternário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha um número");
+            int num = int.Parse(Console.ReadLine());
+            string numero = (num % 2 == 0 ? "é par":"é impar");
+            Console.WriteLine(numero);
+        }
+    }
+}
