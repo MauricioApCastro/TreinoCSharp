e9c9229c8390f0fd446ca769e5c791be9642d4a8
MauricioApCastro <castromac6367@gmail.com>
2022-08-29 22:58:57 -0300

    variáveis

---
+++ Variaveis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual a sua idade?");
+            int idade =int.Parse( Console.ReadLine());
+            Console.WriteLine("Qual a sua altura?");
+            double altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual é o seu sexo?");
+            char sexo = char.Parse(Console.ReadLine());
+            bool corretas = true;
+            bool erradas = false;
+
+            Console.WriteLine("Seu nome é " +nome);
+            Console.WriteLine("Sua idade é " + idade); 
+            Console.WriteLine("Sua altura é " + altura);
+            Console.WriteLine("Você é do sexo " + sexo);
+            Console.WriteLine("Todas as infomações estão corretas?" + corretas);
+            Console.WriteLine("Todas as infomações estão erradas?" + erradas);
+        }
+    }
+}
