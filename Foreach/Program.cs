dc5ff7e3056b2f02be700b1d520f4470224bde00
MauricioApCastro <castromac6367@gmail.com>
2022-08-31 20:35:59 -0300

    Foreach

---
+++ Foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string []nome = {"mauricio","danilo","Silvia"};
+            foreach (string leitura in nome)
+            {
+                Console.WriteLine(leitura);
+            }
+        }
+        
+    }
+}   
