a8f69161b3958cee199b8e5a49ee16376ffbe25a
MauricioApCastro <castromac6367@gmail.com>
2022-08-31 18:43:04 -0300

    exemplo de while

---
+++ While/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero = 0;
+            while (numero <=10)
+            {
+                Console.WriteLine(numero);
+                ++numero;
+                
+            }
+            
+                
+        }
+
+    }
+}
