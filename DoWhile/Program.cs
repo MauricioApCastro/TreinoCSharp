ed06a309a884e69efd9868c07420d38f37b59820
MauricioApCastro <castromac6367@gmail.com>
2022-08-31 19:39:41 -0300

    Exemplo de DoWhile

---
+++ DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            do
+            {
+                Console.WriteLine(numero);
+                numero++;
+            }
+            while (numero <= 5);
+        }
+    }
+}
