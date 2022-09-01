1cf3838436b50cd46a447e6bb9ce7454847a6dd0
MauricioApCastro <castromac6367@gmail.com>
2022-08-31 21:16:37 -0300

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int count = 0;
+            List<string> a = new List<string>();
+            while( count <= 5)
+            {
+                Console.WriteLine("Digite um nome");
+                a.Add(Console.ReadLine());
+                count++;
+            }
+            foreach(string nome in a)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
