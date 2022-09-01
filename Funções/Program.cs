8ac19906e27339aa2c3cb40335609cf66ba504ea
MauricioApCastro <castromac6367@gmail.com>
2022-08-31 21:46:18 -0300

    função

---
+++ Funções/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Funções
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Somar(1, 3));
+            Tabuada();
+
+        }
+        public static int Somar(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public static void Tabuada()
+        {
+            int i = 0;
+            while ( i <= 10)
+            {
+                Console.WriteLine("2 X " + i + "= " + (2 * i));
+                i++;
+            }
+        }
+    }
+}
