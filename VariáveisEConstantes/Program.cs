8582de5b98c8de12655f9ba62bba9a33a993aa1d
MauricioApCastro <castromac6367@gmail.com>
2022-09-04 19:07:09 -0300

    Constantes

---
+++ VariáveisEConstantes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariáveisEConstantes
+{
+    class Program
+    {
+        public const int SAIDA_PROGRAMA = 1;
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("Digite 1 para sair do programa");
+                int numero = int.Parse(Console.ReadLine());
+                if (SAIDA_PROGRAMA == numero)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
