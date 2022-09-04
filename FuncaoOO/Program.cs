89255d24b2be9b27b28fa213cce5e6286fcd73d4
MauricioApCastro <castromac6367@gmail.com>
2022-09-04 20:15:46 -0300

    Exemplo de Funcao com orientacao objeto

---
+++ FuncaoOO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FuncaoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int escolha = 0;
+           
+            switch (escolha)
+            {
+                case 0:
+                    mensagem();
+                    int valor = int.Parse(Console.ReadLine());
+                    break;
+                case 1:
+                    Console.WriteLine("Você escolheu opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu opção 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Você escolheu opção 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Você escolheu opção 4");
+                    break;
+            }
+
+        }
+
+        public static void mensagem()
+        {
+            Console.WriteLine ("Digite uma das opções abaixo:" +
+                     "\n     0 - Sair do Programa" +
+                     "\n     1 - Para ler arquivos" +
+                     "\n     2 - Para executar a tabuada" +
+                     "\n     3 - Calcular média de alunos");
+        }
+    }
+}
+
+
+    
+
+
+    
+
