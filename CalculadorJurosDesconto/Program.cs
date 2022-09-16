df49ae4b6f2a943833ac0aa99128bb0044cf084a
MauricioApCastro <castromac6367@gmail.com>
2022-09-16 19:18:46 -0300

    refectoy

---
+++ CalculadorJurosDesconto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadorJurosDesconto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("informe o valor: ");
+            double valor = double.Parse(Console.ReadLine());
+            Console.Write("informe a porcentagem: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+            double desconto = valor - (valor * (porcentagem / 100));
+            double juros = valor + (valor * (porcentagem / 100));
+
+            Console.WriteLine("R$"+valor + " com R$"+ porcentagem+ " de desconto é R$"+desconto);
+            Console.WriteLine("R$"+valor + " com R$" + porcentagem + " de desconto é R$" + juros);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ console-app/Diretorio/Ler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoOO.Funcoes
+{
+    class Ler
+    {
+        private static string caminhoArquivo()
+        {
+            return ConfigurationManager.AppSettings["BaseClientes"];
+            }
+        public static void Arquivo(int numeroArquivo)
+        {
+          
+            string arquivoComCaminho = caminhoArquivo ()+ "arq" + numeroArquivo + ".txt";
+            if (File.Exists(arquivoComCaminho))
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            string arquivoComCaminho2 = caminhoArquivo() + "arq"  + (numeroArquivo + 1) + ".txt";
+            if ((File.Exists(arquivoComCaminho2)))
+            {
+                Console.WriteLine("================");  
+                Arquivo(numeroArquivo + 1);
+            }
+        }
+    }
+}
