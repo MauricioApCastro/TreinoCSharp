8335608f2f0cf6593aa34e4f3babd6ec59460161
MauricioApCastro <castromac6367@gmail.com>
2022-09-23 17:52:27 -0300

    Calcula area de círculo

---
+++ CalculoAreaCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoAreaCirculo
+{
+    internal class Program
+    {
+        const double PI = 3.14159;
+        static void Main(string[] args)
+        {
+            double raio = double.Parse( Console.ReadLine());
+            double area = PI * (raio * raio);
+            Console.WriteLine(String.Format("{0:00.0000}", area));
+            Console.ReadKey();
+        }
+    }
+}
