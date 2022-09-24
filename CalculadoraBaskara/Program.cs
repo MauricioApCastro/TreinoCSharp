bfaa2d0f45262e33fc3a9db60576e2d2fd9753a9
MauricioApCastro <castromac6367@gmail.com>
2022-09-24 09:56:05 -0300

    App fórmula de Baskara

---
+++ CalculadoraBaskara/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraBaskara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c,delta,x1,x2;
+
+            Console.WriteLine("Digite o valor de a");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de b");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de c");
+            c = double.Parse(Console.ReadLine());
+
+            delta = Math.Pow(b,2.0) - 4.0 * a * c;   
+            x1 = (-b + Math.Sqrt(delta))/(2.0 * a);
+            x2 = (b + Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine("Delta: " + delta);
+            Console.WriteLine("x1 : " + x1);
+            Console.WriteLine("x2 :" + x2);
+                
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ExeVariaveis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritótio";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos");
+            Console.WriteLine($"Computador, cujo preço é $ {preco1:F2} ");
+            Console.WriteLine($"Mesa de escritório, cujo preço é $ {preco2:F2}\n\n");
+            Console.WriteLine($"Registro: 30 anos de idade, código {codigo} e gênero: {genero}\n\n");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado(três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: {0} ",medida.ToString("F3",CultureInfo.InvariantCulture));
+
+
+            Console.ReadKey();
+        }
+    }
+}
