b81dc580f7fe21180b0b6a1827bddfdad70d7ab4
MauricioApCastro <castromac6367@gmail.com>
2022-09-24 20:55:32 -0300

    verificador negativo

---
+++ VerificadorNegativo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificadorNegativo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num  =  int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NÃO-NEGATIVO");
+            }
+            Console.ReadLine();
+        }
+    }
+}
