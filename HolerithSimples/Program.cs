aa074f4dcb4763bbbccd85fb59c696a0993947c8
MauricioApCastro <castromac6367@gmail.com>
2022-09-24 20:30:30 -0300

    holerith

---
+++ HolerithSimples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolerithSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionario;
+            int qtdeHorasTrabalhada;
+            double valorHoraTrabalho, salario;
+
+            numeroFuncionario = int.Parse( Console.ReadLine());
+            qtdeHorasTrabalhada = int.Parse(Console.ReadLine());
+            valorHoraTrabalho = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            salario = qtdeHorasTrabalhada * valorHoraTrabalho;
+
+            Console.WriteLine("NUMBER = "+numeroFuncionario);
+            Console.WriteLine("SALARY = U$ "+salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
