0d4d6882792087259ec6fe52b0ac0465054132f6
MauricioApCastro <castromac6367@gmail.com>
2022-09-24 20:59:59 -0300

    verificador par e impar

---
+++ ParImpar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
