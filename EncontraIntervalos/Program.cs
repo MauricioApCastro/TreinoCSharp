06ea1637cee19a07520189aea5f7031c4f2dde75
MauricioApCastro <castromac6367@gmail.com>
2022-09-25 08:32:53 -0300

    encontra um número em um intervalo

---
+++ EncontraIntervalos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncontraIntervalos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double num = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            if ((num > 0) && (num <= 25))
+            {
+                Console.WriteLine("Intervalo (0.25)");
+            }
+            else if ((num > 25) && (num <= 50))
+            {
+                Console.WriteLine("Intervalo (25.50)");
+            }
+            else if ((num > 50) && (num <= 75))
+            {
+                Console.WriteLine("Intervalo (50.75)");
+            }
+            else if ((num > 75) && (num <= 100))
+            {
+                Console.WriteLine("Intervalo (75.100)");
+            }
+            else
+            {
+                Console.WriteLine("fora de intervalo");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
