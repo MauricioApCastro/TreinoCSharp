b7ba07730137123fb0868ff46079df1c73056184
MauricioApCastro <castromac6367@gmail.com>
2022-09-25 16:58:50 -0300

    Calcula qual a pessoa mais velha

---
+++ MaisVelha/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaisVelha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            pessoa.Nome =  Console.ReadLine();
+            pessoa.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            pessoa2.Nome = Console.ReadLine();
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            if (pessoa.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha :" +pessoa.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha :" + pessoa2.Nome);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
