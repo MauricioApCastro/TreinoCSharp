6a279005238ec679a56747b83d9db0319a0fdcfe
MauricioApCastro <castromac6367@gmail.com>
2022-09-24 21:32:49 -0300

    Duração de jogo

---
+++ DuracaoJogo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuracaoJogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int duracao;
+            string[] numeros = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(numeros[0]);
+            int horaFinal = int.Parse(numeros[1]);
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+            Console.ReadLine();
+        }
+
+    }
+}
+
