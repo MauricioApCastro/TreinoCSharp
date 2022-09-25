12c1af9c1d0cbe7f074c429d7a20569e5c1c9305
MauricioApCastro <castromac6367@gmail.com>
2022-09-24 21:11:05 -0300

    multiplos

---
+++ Multiplo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string[] numeros = Console.ReadLine().Split(' ');
+            int num1 = int.Parse(numeros[0]);
+            int num2 = int.Parse(numeros[1]);
+
+            if ((num1 % num2 == 0)||(num2 % num1 == 0))
+            {
+                Console.WriteLine("São Múltiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não São Múltiplos");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
