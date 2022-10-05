94a26a812aab0fe8d733d414590a7e7cb08b3d34
MauricioApCastro <castromac6367@gmail.com>
2022-10-04 21:14:03 -0300

    Programa que mostra se um aluno foi aprovado ou não com Orientação Objeto

---
+++ AprovacaoAluno/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprovacaoAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota;
+
+
+        public void SomaNota(double nota)
+        {
+            Nota += nota;          
+        }
+        
+
+        public void Avaliador()
+        {
+            if(Nota >= 60.00)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60.00 - Nota) + " PONTOS");
+            }
+        }
+
+       
+
+    }
+}
---
+++ AprovacaoAluno/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprovacaoAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            for (int i = 0; i < 3; i ++) 
+            {
+                double nota = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                aluno.SomaNota(nota);
+            
+            }
+
+            Console.WriteLine("NOTA FINAL ="+aluno.Nota.ToString("F2",CultureInfo.InvariantCulture));
+            
+            aluno.Avaliador();
+            Console.ReadLine();
+            
+        }
+    }
+}
