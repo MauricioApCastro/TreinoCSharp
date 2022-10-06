9ce83a26ca15f5ce831794410c86c3cf3bb08ccb
MauricioApCastro <castromac6367@gmail.com>
2022-10-05 21:54:36 -0300

    Programa de conversão

---
+++ Conversor/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+
+        public static double Conversor(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * Iof / 100;
+        }
+    }
+}
---
+++ Conversor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);         
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantiaEmDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          
+            double total = ConversorDeMoeda.Conversor(quantiaEmDolar,cotacao);
+            Console.WriteLine("Valor a ser pago em reais = " +total.ToString("F2",CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
