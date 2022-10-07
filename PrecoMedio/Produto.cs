299b4def112d9cc34ffe9cf15f2e8f39775e61dd
MauricioApCastro <castromac6367@gmail.com>
2022-10-07 19:10:01 -0300

    Calcula o preço médio utilizando Vetor

---
+++ PrecoMedio/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecoMedio
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+
+    }
+}
---
+++ PrecoMedio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecoMedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            int n = int.Parse(Console.ReadLine());
+            Produto[]dados= new Produto[n];
+            for (int i = 0;i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                dados[i] = new Produto{ Nome = nome,Preco = preco};
+                soma += dados[i].Preco;
+            }
+            
+            double media = soma / 3;
+            Console.WriteLine("Preço Médio = "+media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
