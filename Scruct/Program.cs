c56de196b227f38e6997edf92a49ec24e2550a6e
MauricioApCastro <castromac6367@gmail.com>
2022-10-07 18:02:03 -0300

    Exemplo de função struct

---
+++ Scruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aprendendo;
+
+namespace Scruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            //obriga a inicialização das variáveis;
+            Console.WriteLine(p);
+            Console.ReadLine();
+
+        }
+    }
+}
