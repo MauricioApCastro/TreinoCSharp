4577acc5094ec7a89a5fe6cceb8cc0a217f7caec
MauricioApCastro <castromac6367@gmail.com>
2022-10-08 14:02:55 -0300

    uso do params

---
+++ Params/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    class Calculadora
+    {
+        //funçao que automatiza a quantidade de parametros
+        public static int Soma(params int[] numbers)
+        {
+            int soma = 0;
+            for (int i = 0;i < numbers.Length; i++)
+            {
+                soma += numbers[i];
+            }
+            return Soma();
+            
+        } 
+    }
+}
---
+++ Params/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculadora.Soma(5, 6);
+
+            Console.WriteLine(s1);
+            Console.ReadKey();
+
+        }
+    }
+}
